159a31498bc5e97806d51ad005bce2a439850a05
Jelica <jelicastanojevicc@gmail.com>
2021-07-23 17:46:27 +0200

    <style>: Identation

--- src/schema/typeDefs/postType.js
+++ src/schema/typeDefs/postType.js
@@ -1,39 +1,39 @@
-import { gql } from "apollo-server";
+import { gql } from 'apollo-server';
 
 export const postTypeDefs = gql`
-	type Post {
-		id: ID!
-		title: String!
-		content: String!
-		userID: ID!
-		comments: [Comment]
-	}
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    userID: ID!
+    comments: [Comment]
+  }
 
-	type Comment {
-		content: String!
-		userID: ID!
-	}
-	
-    input PostInput {
-        content: String
-        title: String
-		user_id: ID
-    }
+  type Comment {
+    content: String!
+    userID: ID!
+  }
 
-	input CommentInput {
-        content: String
-		user_id: ID
-    }
+  input PostInput {
+    content: String
+    title: String
+    user_id: ID
+  }
 
-	extend type Query {
-        posts: [Post]
-		post(id: ID!): Post!
-	}
+  input CommentInput {
+    content: String
+    user_id: ID
+  }
 
-	extend type Mutation {
-		createPost(postInput: PostInput): Post
-        updatePost(id: ID!, postInput: PostInput): Post
-		deletePost(id: ID!): Post
-		addComment(postId: ID!, commentInput: CommentInput): Comment
-	}
+  extend type Query {
+    posts: [Post]
+    post(id: ID!): Post!
+  }
+
+  extend type Mutation {
+    createPost(postInput: PostInput): Post
+    updatePost(id: ID!, postInput: PostInput): Post
+    deletePost(id: ID!): Post
+    addComment(postId: ID!, commentInput: CommentInput): Comment
+  }
 `;
